c375c659dc2713fe70f8ea5e0cf3bc3ca7ca09c0
Simon J Stuart <lakraven@gmail.com>
2026-09-14 12:10:20 +0200

    Expose optional Primitive discovery through Lua umbrella

--- src/ESPressio_Lua.hpp
+++ src/ESPressio_Lua.hpp
@@ -1,3 +1,7 @@
 #pragma once
 /// <summary>Public ESPressio Lua 5.5.1 type-binding and scripting-instance API.</summary>
 #include "ESPressio_LuaInstance.hpp"
+
+#if __has_include(<ESPressio_TypeDirectory.hpp>)
+#include "ESPressio_LuaPrimitiveDiscovery.hpp"
+#endif
